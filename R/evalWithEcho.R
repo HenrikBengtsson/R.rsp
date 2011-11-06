e43f5b7d29308343591e23e9eddf5eccc2c53a81
Henrik Bengtsson <hb@aroma-project.org>
2011-11-06 19:28:53 +0000

    Version: 0.6.7 [2011-11-06] o Updated code and vignettes to utilize evalCapture() of R.utils v1.9.2. o DEPRECATED: evalWithEcho(); use evalCapture() instead.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@650 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/evalWithEcho.R
+++ R/evalWithEcho.R
@@ -1,35 +1,14 @@
-evalWithEcho <- function(expr, ..., envir=parent.frame(), trim=TRUE, collapse="\n") {
-  # Get code/expression without evaluating it
-  expr2 <- substitute(expr);
-  code <- capture.output(print(expr2));
-
-  # Trim of surrounding { ... }
-  if (code[1] == "{") {
-    code <- code[-c(1, length(code))];
-  }
-
-  # Evaluate the code via source()
-  con <- textConnection(code, open="r");
-  res <- capture.output({
-    sourceTo(file=con, echo=TRUE, ..., envir=envir);
-  });
-
-  # Drop empty lines
-  if (trim) {
-    res <- res[nchar(res) > 0];
-  }
-
-  if (!is.null(collapse)) {
-    res <- c(res, "");
-    res <- paste(res, collapse=collapse);
-  }
-
-  invisible(res);
-} # evalWithEcho()
+evalWithEcho <- function(expr, ...) {
+  evalCapture(expr, ..., code=TRUE, output=TRUE);
+}
 
 
 ##############################################################################
 # HISTORY:
+# 2011-11-05
+# o DEPRECATED: evalWithEcho(); use evalCapture().
+# o Added evalCapture(..., code=TRUE, output=TRUE).
+# o evalc() is an alias for evalCapture().
 # 2011-03-28
 # o Rewrote evalWithEcho() so that it utilizes source(..., echo=TRUE).
 # o BUG FIX: evalWithEcho() would only add the prompt to the first line.
