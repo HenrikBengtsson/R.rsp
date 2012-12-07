fbed3ae958e55b2dc378808550c07b4c3b67f64d
Henrik Bengtsson <hb@aroma-project.org>
2012-12-07 04:42:36 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@957 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/rsp,md.R
+++ tests/rsp,md.R
@@ -1,7 +1,7 @@
 library("R.rsp")
 
-pathname <- system.file("exData", "LoremIpsum.html.rsp", package="R.rsp")
+pathname <- system.file("exData", "LoremIpsum.md.rsp", package="R.rsp")
 pathname <- Arguments$getReadablePathname(pathname)
-pathnameR <- rsp(pathname, outPath="html.rsp", verbose=-10)
+pathnameR <- rsp(pathname, outPath="md.rsp", verbose=-10)
 print(pathnameR)
 pathnameR <- Arguments$getReadablePathname(pathnameR)
