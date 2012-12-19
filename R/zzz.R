191e8ce428a437fd370de44552be82d9c7dd4c65
Henrik Bengtsson <hb@aroma-project.org>
2012-12-19 13:27:15 +0000

    Version: 0.8.4 [2012-12-19] o Utilizing new startupMessage() of R.oo.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@972 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,7 @@
 .onAttach <- function(libname, pkgname) {  
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
-
-  packageStartupMessage(getName(pkg), " v", getVersion(pkg), " (", 
-    getDate(pkg), ") successfully loaded. See ?", pkgname, " for help.\n",
-    " Type browseRsp() to open the RSP main menu in your browser.");
+  startupMessage(pkg);
 }
 
 
