411afa50de58562700536c3f54812fa031749906
Henrik Bengtsson <hb@aroma-project.org>
2013-01-21 00:48:25 +0000

    Version: 0.8.5 [2013-01-20] Added compileKnitr() and corresponding system tests.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@1006 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/compileKnitr.R
+++ tests/compileKnitr.R
@@ -1,11 +1,11 @@
 library("R.rsp")
 
-pathname <- system.file("exData", "LoremIpsum.tex", package="R.rsp")
+pathname <- system.file("exData", "LoremIpsum.knitr.Rnw", package="R.rsp")
 pathname <- Arguments$getReadablePathname(pathname)
 print(pathname)
 
 if (Sys.getenv("_R_CHECK_FULL_") != "") {
-  pathnameR <- compileLaTeX(pathname, outPath="tex/", verbose=-10)
+  pathnameR <- compileKnitr(pathname, outPath="knitr.Rnw/", verbose=-10)
   print(pathnameR)
   pathnameR <- Arguments$getReadablePathname(pathnameR)
 }
