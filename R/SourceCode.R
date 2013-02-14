3216de0288142a51eb37e85981d8c2186173061b
Henrik Bengtsson <hb@aroma-project.org>
2013-02-14 03:11:31 +0000

    Forgot to commit a file.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@1039 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/SourceCode.R
@@ -0,0 +1,125 @@
+###########################################################################/**
+# @RdocClass SourceCode
+#
+# @title "The SourceCode class"
+#
+# \description{
+#  @classhierarchy
+#
+#  An SourceCode object is a @character @vector holding source code for
+#  a particular programming language.
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{code, ...}{@character strings.}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods
+# }
+# 
+# @author
+#*/###########################################################################
+setConstructorS3("SourceCode", function(code=character(), ...) {
+  extend(c(code, ...), "SourceCode");
+})
+
+
+#########################################################################/**
+# @RdocMethod print
+#
+# @title "Prints the source code"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Not used.}
+# }
+#
+# \value{
+#  Returns nothing.
+# }
+#
+# @author
+#
+# \seealso{
+#   @seeclass
+# }
+#*/######################################################################### 
+setMethodS3("print", "SourceCode", function(x, ...) {
+  code <- paste(x, collapse="\n");
+  cat(code);
+  cat("\n");
+})
+
+
+#########################################################################/**
+# @RdocMethod parse
+#
+# @title "Parses the source code"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Optional arguments passed to language parse.}
+# }
+#
+# \value{
+#  Returns an @expression.
+# }
+#
+# @author
+#
+# \seealso{
+#   @seeclass
+# }
+#*/######################################################################### 
+setMethodS3("parse", "SourceCode", abstract=TRUE);
+
+
+#########################################################################/**
+# @RdocMethod evaluate
+#
+# @title "Parses and evaluates the source code"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Optional arguments passed to @seemethod "parse".}
+# }
+#
+# \value{
+#  Returns the last evaluated expression, iff any.
+# }
+#
+# @author
+#
+# \seealso{
+#   @seeclass
+# }
+#*/######################################################################### 
+setMethodS3("evaluate", "SourceCode", abstract=TRUE);
+
+
+
+##############################################################################
+# HISTORY:
+# 2013-02-11
+# o Added Rdoc help.
+# 2013-02-09
+# o Created.
+##############################################################################
