18474f3ccfc74e57d3009ce780178d5002bdf3f4
Henrik Bengtsson <hb@aroma-project.org>
2013-03-18 17:01:37 +0000

    Added RspException class, which will be the root class of all RSP exception.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@1289 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/RspException.R
@@ -0,0 +1,69 @@
+###########################################################################/**
+# @RdocClass RspException
+#
+# @title "The RspException class"
+#
+# \description{
+#  @classhierarchy
+#
+#  An RspException is an @see "R.oo::Exception" that is thrown during
+#  the processing of an RSP document.
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{str}{A @character string.}
+#   \item{...}{Not used.}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods
+# }
+# 
+# @author
+#
+# @keyword internal
+#*/###########################################################################
+setConstructorS3("RspException", function(...) {
+  extend(Exception(...), "RspException");
+})
+
+setMethodS3("as.character", "RspException", function(x, ...) {
+  s <- NextMethod("as.character");
+  s;
+})
+
+setConstructorS3("RspPreprocessingException", function(..., item=NULL) {
+  extend(RspException(...), "RspPreprocessingException",
+    item = item
+  );
+})
+
+setMethodS3("getMessage", "RspPreprocessingException", function(this, ...) {
+  ## The following is not possible due to bug in R.oo 1.13.0:
+  ##  msg <- NextMethod("getMessage");
+  msg <- this$.msg;
+
+  item <- this$item;
+  if (!is.null(item)) {
+    itemStr <- asRspString(item);
+    itemStr <- sprintf(" (%s)", itemStr);
+  } else {
+    itemStr <- "";
+  }
+  sprintf("An error occured while preprocessing RSP directive%s: %s", itemStr, msg);
+})
+
+setMethodS3("getItem", "RspPreprocessingException", function(this, ...) {
+  this$item;
+})
+
+
+
+
+##############################################################################
+# HISTORY:
+# 2013-03-17
+# o Created.
+##############################################################################
