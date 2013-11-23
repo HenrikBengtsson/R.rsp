551c4fde846cc94fb6fa843beafb35d32f06f0b8
Henrik Bengtsson <hb@aroma-project.org>
2013-11-23 21:53:18 +0000

    Version: 0.10.2 [2013-11-23] o BUG FIX: rsource() would not evaluate in the current environment.   Added a package system test for this.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@1854 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rsource.R
+++ R/rsource.R
@@ -1,29 +1,31 @@
-setMethodS3("rsource", "default", function(file, path=NULL, output="", buffered=FALSE, ...) {
-  rcat(file=file, path=path, output=output, buffered=buffered, ...);
+setMethodS3("rsource", "default", function(file, path=NULL, envir=parent.frame(), output="", buffered=FALSE, ...) {
+  rcat(file=file, path=path, envir=envir, output=output, buffered=buffered, ...);
 }) # rsource()
 
-setMethodS3("rsource", "RspString", function(rstr, output="", buffered=FALSE, ...) {
-  rcat(rstr, output=output, buffered=buffered, ...);
+setMethodS3("rsource", "RspString", function(rstr, envir=parent.frame(), output="", buffered=FALSE, ...) {
+  rcat(rstr, envir=envir, output=output, buffered=buffered, ...);
 }, protected=TRUE) # rsource()
 
 
 
-setMethodS3("rsource", "RspDocument", function(doc, output="", buffered=FALSE, ...) {
-  rcat(doc, output=output, buffered=buffered, ...);
+setMethodS3("rsource", "RspDocument", function(doc, envir=parent.frame(), output="", buffered=FALSE, ...) {
+  rcat(doc, envir=envir, output=output, buffered=buffered, ...);
 }, protected=TRUE) # rsource()
 
 
-setMethodS3("rsource", "RspRSourceCode", function(rcode, output="", buffered=FALSE, ...) {
-  rcat(rcode, output=output, buffered=buffered, ...);
+setMethodS3("rsource", "RspRSourceCode", function(rcode, envir=parent.frame(), output="", buffered=FALSE, ...) {
+  rcat(rcode, envir=envir, output=output, buffered=buffered, ...);
 }, protected=TRUE) # rsource()
 
-setMethodS3("rsource", "function", function(object, output="", buffered=FALSE, ...) {
-  rcat(object, output=output, buffered=buffered, ...);
+setMethodS3("rsource", "function", function(object, envir=parent.frame(), output="", buffered=FALSE, ...) {
+  rcat(object, envir=envir, output=output, buffered=buffered, ...);
 }, protected=TRUE) # rsource()
 
 
 ############################################################################
 # HISTORY:
+# 2013-11-23
+# o BUG FIX: rsource() would not evaluate in the current environment.
 # 2013-08-05
 # o Created.
 ############################################################################
