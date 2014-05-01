467ae7e0152687b48de2f6c52feada7a32fa9958
Henrik Bengtsson <hb@aroma-project.org>
2014-05-01 07:29:46 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.rsp@2113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/999.package.R
+++ R/999.package.R
@@ -10,14 +10,20 @@
 # }
 #
 # \section{To get started}{
-#   To get started, see:
+#   We recommend that you start by reading one of the '\href{../doc/index.html}{vignettes}';
 #   \enumerate{
-#     \item For a detailed description of RSP, see vignette '\href{../doc/index.html}{Dynamic document creation using RSP}'.
-#     \item For a quick overview of RSP, see vignette '\href{../doc/index.html}{Introductory slides on RSP}'.
-#     \item For a one-page overview of RSP, see vignette '\href{../doc/index.html}{RSP Markup Language - Reference Card}'.
-#     \item To included RSP vignettes in a package, see '\href{../doc/index.html}{Include RSP and other non-Sweave vignettes in R packages}'.
-#     \item To compile a RSP-embedded text document to a final document, use @see "rfile", e.g. \code{rfile("report.tex.rsp")} outputs file 'report.pdf' and \code{rfile("report.md.rsp")} outputs file 'report.html'.
-#     \item To compile a RSP document (file or string) and output the result to standard output or a string, use @see "rcat" an @see "rstring", respectively.
+#     \item A 5 minute slideshow covering the basics of RSP.
+#     \item Detailed description of the RSP markup language.
+#     \item A one-page RSP reference card.
+#     \item How to use RSP for package vignettes.
+#     \item How to use static PDF or HTML package vignettes.
+#   }
+#
+#   Then, when you're ready to try it yourself, these are commands you can start with:
+#   \enumerate{
+#     \item Play with @see "rcat", which works like @see "base::cat" but also processed RSP expressions, e.g. \code{rcat("A random number: <\%=sample(100, size=1)\%>\n")}.
+#     \item To source a RSP document as you do with R scripts, use @see "rsource", e.g. \code{rsource("report.md.rsp")} which will run the RSP and display the output as it appears.
+#     \item To compile a RSP document to a final document, use @see "rfile", e.g. \code{rfile("report.md.rsp")} outputs Markdown file 'report.md' which is automatically compiled into a final 'report.html'.
 #   }
 # }
 #
