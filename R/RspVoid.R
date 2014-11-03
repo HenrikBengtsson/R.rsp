d844859eff50611949b679a70117af70d6c300cf
hb <hb@aroma-project.org>
2014-11-02 20:06:15 -0800

    Typo

--- R/RspVoid.R
+++ R/RspVoid.R
@@ -2,7 +2,7 @@ setConstructorS3("RspVoid", function(...) {
   extend(RspConstruct(), "RspVoid")
 })
 
-setMethodS3("asRspString", "RspComment", function(object, ...) {
+setMethodS3("asRspString", "RspVoid", function(object, ...) {
   RspString()
 })
 
