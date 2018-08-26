5004259d43d6dedf0cbf060471749db41d3e234d
Henrik Bengtsson <hb@aroma-project.org>
2018-08-25 20:23:48 -0700

    Replace deprecated evalWithTimeout() with withTimeout()

--- tests/include,recursive.R
+++ tests/include,recursive.R
@@ -4,7 +4,7 @@ path <- system.file("rsp_tests", package="R.rsp")
 pathname <- file.path(path, "recursive.txt.rsp")
 print(pathname)
 
-R.utils::evalWithTimeout({
+R.utils::withTimeout({
   res <- try({ rcat(file=pathname) })
   stopifnot(inherits(res, "try-error"))
 }, timeout=30L)
