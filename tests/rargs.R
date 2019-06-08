0b5767bc997c572094100d03052ba4dbec357564
Henrik Bengtsson <hb@aroma-project.org>
2019-06-07 21:55:05 -0700

    Make rargs() work also without vignettes built, e.g. AppVeyor CI

--- tests/rargs.R
+++ tests/rargs.R
@@ -1,10 +1,10 @@
 library("R.rsp")
 
-path <- system.file("doc", package="R.rsp")
+path <- system.file("rsp_examples", package="R.rsp")
 print(path)
 print(dir(path=path))
 
-args <- rargs(file="RSP_refcard.tex.rsp", path=path)
+args <- rargs(file="LoremIpsum.tex.rsp", path=path)
 print(args)
 str(args)
 
